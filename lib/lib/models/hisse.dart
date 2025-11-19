3be447820b7b4e5dc08f7022ba303ceffe69feb5
furkankoyunlu26-sys <furkankoyunlu26@icloud.com>
2025-11-19 04:18:00 +0300

    Create Hisse model class with properties

---
+++ lib/lib/models/hisse.dart
@@ -0,0 +1,15 @@
+class Hisse {
+  final String id;
+  final String ownerName;
+  final double kilo;
+  final bool isPaid;
+  final bool isDelivered;
+
+  Hisse({
+    required this.id,
+    required this.ownerName,
+    required this.kilo,
+    required this.isPaid,
+    required this.isDelivered,
+  });
+}
